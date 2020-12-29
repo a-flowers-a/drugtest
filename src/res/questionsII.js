5fc40c405b51cd934c9629cfa08cec503b6d3cc0
alejandro <alejandro@alejandros-MacBook-Pro.local>
2020-12-29 14:10:18 -0600

    transcript second part questions

---
+++ src/res/questionsII.js
@@ -0,0 +1,49 @@
+const questionsII = [
+    {
+        q: "En los últimos 3 meses, ¿fumaste un cigarrillo que contenía tabaco?",
+        a: "En los últimos 3 meses, ¿fumabas más de 10 cigarrillos al día?",
+        b: "En los últimos 3 meses, ¿fumabas normalmente dentro de los 30 minutos después de despertar?"
+    },
+    {
+        q: "En los últimos 3 meses, ¿bebiste alcohol? * Una bebida estándar es aproximadamente 1 vaso pequeño de vino (5 onzas), 1 cerveza (12 onzas) o 1 solo trago de licor.",
+        a: "En los últimos 3 meses, ¿tomaste 4/5 o más bebidas que contienen alcohol en un día? * (Nota: HOMBRE 5 / MUJER 4).",
+        b: "En los últimos 3 meses, ¿has tratado de controlar, reducir o dejar de beber?",
+        c: "En los últimos 3 meses, ¿alguien ha expresado preocupación por tu consumo de alcohol?"
+    },
+    {
+        q: "En los últimos 3 meses, ¿usaste marihuana (hachís, hierba)?",
+        a: "En los últimos 3 meses, ¿has tenido un fuerte deseo o ganas de consumir marihuana al menos una vez a la semana o más a menudo?",
+        b: "En los últimos 3 meses, ¿alguien ha expresado preocupación acerca de tu uso de la marihuana?",
+    },
+    {
+        q: "En los últimos 3 meses, ¿consumiste cocaína, crack o metanfetamina (metanfetamina de cristal)?",
+        a: "En los últimos 3 meses, ¿consumiste cocaína, crack o metanfetamina (metanfetamina de cristal) al menos una vez a la semana o con más frecuencia?",
+        b: "En los últimos 3 meses, ¿alguien ha expresado preocupación por tu consumo de cocaína, crack o metanfetamina (metanfetamina de cristal)?",
+    },
+    {
+        q: "En los últimos 3 meses, ¿usaste heroína?",
+        a: "En los últimos 3 meses, ¿has tratado de controlar, reducir o dejar de consumir heroína?",
+        b: "En los últimos 3 meses, ¿alguien ha expresado preocupación por tu consumo de heroína?",
+    },
+    {
+        q: "En los últimos 3 meses, ¿utilizaste un analgésico opiáceo con receta médica (por ejemplo, Percocet, Vicodin) no de acuerdo a lo que se te prescribió o que no se te prescribió en absoluto?",
+        a: "En los últimos 3 meses, ¿has tratado de controlar, reducir o dejar de usar un analgésico opiáceo?",
+        b: "En los últimos 3 meses, ¿alguien ha expresado preocupación por tu uso de un analgésico opiáceo?",
+    },
+    {
+        q: "En los últimos 3 meses, ¿utilizaste un medicamento para la ansiedad o el sueño (por ejemplo, Xanax, Ativan, o Klonopin) no según lo prescrito o que no se prescribió para ti?",
+        a: "¿En los últimos 3 meses, has tenido un fuerte deseo o necesidad de usar medicamentos para la ansiedad o el sueño al menos una vez a la semana o con más frecuencia?",
+        b: "En los últimos 3 meses, ¿alguien ha expresado preocupación acerca de tu uso de medicamentos para la ansiedad o el sueño?",
+    },
+    {
+        q: "En los últimos 3 meses, ¿utilizaste un medicamento para el TDAH (por ejemplo, Adderall, Ritalin) no según lo prescrito o que no se prescribió para ti?",
+        a: "En los últimos 3 meses, ¿utilizaste un medicamento para el TDAH (por ejemplo, Adderall, Ritalin) al menos una vez a la semana o más a menudo?",
+        b: "En los últimos 3 meses, ¿alguien ha expresado preocupación acerca de tu uso de un medicamento para el TDAH (por ejemplo, Adderall o Ritalin)?",
+    },
+    {
+        q: "En los últimos 3 meses, ¿usaste alguna otra droga ilegal o recreativa (por ejemplo, éxtasis/éxtasis, GHB, poppers, LSD, hongos, K especial, sales de baño, marihuana sintética ('spice'), whip-its, etc.)?",
+        a: "En los últimos 3 meses, ¿cuáles fueron las otras drogas(s) que usaste?",
+    },
+];
+
+export default questionsII;
