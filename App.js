69851c85640f7dd471a548be6c3951133bfc11f1
Brees-T <37353793+Brees-T@users.noreply.github.com>
2021-01-10 22:00:31 -0600

    Added app as a share option on the whatsapp menu

--- App.js
+++ App.js
@@ -6,37 +6,92 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import AnalysisStack from 'drugtest/src/components/AnalysisStack';
+import ShareMenu from 'react-native-share-menu';
 
 const Tabs = createBottomTabNavigator();
 
-const App = () => {
+type SharedItem = {
+  mimeType: string,
+  data: string,
+};
+
+
+/*----------------------------------*/
+const App: () => React$Node = () => {
+  /*This is to receive the file(chat) */
+  const [sharedData, setSharedData] = useState('');
+  const [sharedMimeType, setSharedMimeType] = useState('');
+  const [sharedExtraData, setSharedExtraData] = useState(null);
+
+  const handleShare = useCallback((item: ?SharedItem) => {
+    if (!item) {
+      return;
+    }
+
+    const { mimeType, data, extraData } = item;
+
+    setSharedData(data);
+    setSharedExtraData(extraData);
+    setSharedMimeType(mimeType);
+
+  }, []);
+
+  useEffect(() => {
+    ShareMenu.getInitialShare(handleShare);
+  }, []);
+
+  useEffect(() => {
+    const listener = ShareMenu.addNewShareListener(handleShare);
+
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+  var getSharedChat = () => {
+    if (sharedMimeType) {
+      console.log("There is a MimeType: " + sharedMimeType);
+      console.log(JSON.stringify(sharedData));//This retrieves the URI of the chat 
+    }
+    else {
+      console.log("there is nothing to share");
+    }
+  }
+
+  getSharedChat();
+
   return (
+
     <NavigationContainer>
+
       <Tabs.Navigator
         tabBarOptions={{
           tintColor: "#fefefe",
-          style:{
+          style: {
             backgroundColor: "#010101"
           },
         }}
+
       >
         <Tabs.Screen
           component={AnalysisStack}
           name="Analysis"
           options={{
-            tabBarIcon: ({size, color}) => (
+            tabBarIcon: ({ size, color }) => (
               <Image
                 source={require('drugtest/src/assets/bank.png')}
-                style={{ tintColor: color, width: size, height: size}}
+                style={{ tintColor: color, width: size, height: size }}
               />
             )
           }}
         />
+
+
 
         {/*<Tabs.Screen
           component={nameOfTheStackComponent}
@@ -51,8 +106,12 @@ const App = () => {
           }}
         />*/}
 
+
+
       </Tabs.Navigator>
     </NavigationContainer>
+
+
   );
 };
 
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import ActionBtn from '../components/ActionBtn';
 
-function HomeScreen(props){
+
+function HomeScreen(props) {
     const styles = StyleSheet.create({
         container: {
             backgroundColor: "#120078",/*120078 */
@@ -11,12 +12,12 @@ function HomeScreen(props){
         },
     });
 
-    function sendToQuestScreen(){
+    function sendToQuestScreen() {
         //must be the name that's in Stack.Screen
         props.navigation.navigate('Questionaire');
     }//handlePress
 
-    return(
+    return (
         <ScrollView style={styles.container}>
             <ActionBtn
                 btnText={"Realizar Cuestionario"}
