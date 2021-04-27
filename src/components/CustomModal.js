0077b013bd2e935e42d42aa8b3626126f0726aa2
alejandro <alejandro@alejandros-MacBook-Pro.local>
2021-04-27 12:55:45 -0500

    remove autocapitalize for password when deleteing account

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -5,78 +5,6 @@ import ActionBtn from './ActionBtn';
 
 function CustomModal(props){
 
-    const styles = StyleSheet.create({
-        acceptBtn:{
-            backgroundColor: "#79d70f", //afa939 91b029 79d70f cdc733
-            borderWidth: 0,
-            borderRadius: 8,
-            marginHorizontal: 10,
-            width: 120,
-            shadowColor: "#000",
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: 0.23,
-            shadowRadius: 2.62,
-            elevation: 4,
-        },
-        cancelBtn:{
-            backgroundColor: "#fa1616",//ec0101 e40017 ff5722 ec4646 ff414d
-            borderRadius: 8,
-            borderWidth: 0,
-            marginHorizontal: 10,
-            width: 120,
-            shadowColor: "#000",
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: 0.23,
-            shadowRadius: 2.62,
-            elevation: 4,
-        },
-        card: {
-            backgroundColor: "#f5f4f4",/*120078 */
-            borderRadius: 7,
-            padding: 15,
-            marginHorizontal: 20,
-        },
-        errorText:{
-            color: "#fa1616",
-            fontSize: 16,
-            textAlign: "left",
-            marginHorizontal: 20,
-            marginBottom: 15,
-        },
-        input: {
-            borderRadius: 5,
-            borderStyle: "solid",
-            borderWidth: 1,
-            borderColor: "black",
-            color: "black",
-            fontSize: 18,
-            marginHorizontal: 15,
-            marginBottom: 5,
-            padding: 10,
-        },
-        inModalContainer:{
-            alignItems: "center",
-            justifyContent: 'center',
-            backgroundColor:"rgba(2, 6, 28, 0.7)",
-            flex:1,
-        },
-        row:{
-            flexDirection: "row",
-            justifyContent: "center"
-        },
-        text: {
-            color: "#2d132c",
-            fontSize: 20,
-            textAlign: "center",
-            margin: 10,
-        },
-    });
     const {
         onAcceptFunc, onCancelFunc,
         text, input, inputName, 
@@ -97,6 +25,7 @@ function CustomModal(props){
                                 control={control}
                                 render={({ onChange, onBlur, value }) => (
                                     <TextInput
+                                        autoCapitalize={password && "none"}
                                         onBlur={onBlur}
                                         keyboardType= {numericInp && 'numeric'}
                                         secureTextEntry={password}
@@ -130,5 +59,78 @@ function CustomModal(props){
         </Modal>
     );
 }//TopAlert
+
+const styles = StyleSheet.create({
+    acceptBtn:{
+        backgroundColor: "#79d70f", //afa939 91b029 79d70f cdc733
+        borderWidth: 0,
+        borderRadius: 8,
+        marginHorizontal: 10,
+        width: 120,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+    },
+    cancelBtn:{
+        backgroundColor: "#fa1616",//ec0101 e40017 ff5722 ec4646 ff414d
+        borderRadius: 8,
+        borderWidth: 0,
+        marginHorizontal: 10,
+        width: 120,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+    },
+    card: {
+        backgroundColor: "#f5f4f4",/*120078 */
+        borderRadius: 7,
+        padding: 15,
+        marginHorizontal: 20,
+    },
+    errorText:{
+        color: "#fa1616",
+        fontSize: 16,
+        textAlign: "left",
+        marginHorizontal: 20,
+        marginBottom: 15,
+    },
+    input: {
+        borderRadius: 5,
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: "black",
+        color: "black",
+        fontSize: 18,
+        marginHorizontal: 15,
+        marginBottom: 5,
+        padding: 10,
+    },
+    inModalContainer:{
+        alignItems: "center",
+        justifyContent: 'center',
+        backgroundColor:"rgba(2, 6, 28, 0.7)",
+        flex:1,
+    },
+    row:{
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    text: {
+        color: "#2d132c",
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+    },
+});
 
 export default CustomModal;
