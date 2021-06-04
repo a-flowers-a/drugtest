ed610cc2641b6b8819a3444ec8b981d382d289ca
alejandro <alejandro@alejandros-MacBook-Pro.local>
2021-06-03 19:01:26 -0500

    text in what we do

--- src/views/WhatWeDo.js
+++ src/views/WhatWeDo.js
@@ -8,8 +8,8 @@ function HomeScreen() {
 
     return (
         <ScrollView style={styles.container}>
-            <Text style={styles.text}>Tu información no es divulgada con alguien más, el contenido de tus chats no es leído ni siquiera por los desarrolladores de esta aplicación, dicho contenido será almacenado en nuestra base de datos hasta que se haya terminado el análisis, una vez concluido, nosotros no tenemos razón para continuar guardando esa información, por lo que el contenido es borrado de nuestra base de datos. Así mismo, te informamos que no almacenamos nombres propios ni de personas ni lugares, ningún dato encontrado en tu chat será almacenado, únicamente el resultado del análisis para poder mostrártelo en tu cuenta.</Text>
-            <Text style={styles.text}>Igualmente te informamos que tus resultados pueden tomados para la obtención de estadísticas en tu Unidad Académica, no obstante, no se mostrarán tus datos personales; es decir, en estas estadísticas no se proporcionarán: tu nombre, boleta, correo ni mucho menos tus credenciales de la aplicación.</Text>
+            <Text style={styles.text}>El contenido de tus chats no es leído ni siquiera por los desarrolladores de esta aplicación, a algunos segmentos de chat le será analizado el sentimiento con Google Natural Language. El contenido del chat será almacenado cifrado (AES-256) en nuestra base de datos. Así mismo, te informamos que no almacenamos nombres propios ni de personas ni lugares; ningún dato encontrado en tu chat será almacenado, únicamente el resultado del análisis para poder mostrártelo en tu cuenta.</Text>
+            <Text style={styles.text}>Igualmente, te informamos que tus resultados pueden tomados para la obtención de estadísticas en tu Unidad Académica. No obstante, no se mostrarán tus datos personales; es decir, en estas estadísticas no se proporcionarán: tu nombre, boleta, correo, ni mucho menos tus credenciales de la aplicación.</Text>
             <View style={styles.UrlView}>
                 <UrlButton />
             </View>
